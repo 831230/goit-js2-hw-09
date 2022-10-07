425e8824768de7019bf22fcabac6b0a7b2cafbb8
831230 <97232146+831230@users.noreply.github.com>
2022-10-07 10:58:22 +0000

    Deploying to gh-pages from @ 831230/goit-js2-hw-09@43603380138cf6eb3afe2d901f5ac5356f6eeab8 🚀

---
+++ 01-color-switcher.f1565529.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");var n=null;t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));e.style.backgroundColor=t}),1e3),t.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.f1565529.js.map
