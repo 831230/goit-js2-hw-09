bd1446d91cf88ead8dc9c95d3a19cc6e843faaa4
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-10-08 17:17:45 +0200

    completed task no 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  
+  const promise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    },delay)
+  });
+  promise
+    .then(value=>Notiflix.Notify.success(value))
+    .catch(err=>Notiflix.Notify.failure(err))
+};
+
+function operatePromises(evt){
+  evt.preventDefault();
+  let delay=Number(form.delay.value);
+  for (let position = 1; position <= form.amount.value; position++) {
+    createPromise(position,delay);
+    delay+=Number(form.step.value);
   }
-}
+};
+
+form.addEventListener("submit", operatePromises)
